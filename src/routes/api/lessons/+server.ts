24f4e4792d3593e0d5dd906b026b17f6822132d5
Ovidiu Căpraru <85124797+ovidiuux@users.noreply.github.com>
2025-03-23 00:42:29 +0200

    Update +server.ts

--- src/routes/api/lessons/+server.ts
+++ src/routes/api/lessons/+server.ts
@@ -4,7 +4,27 @@ import type { RequestHandler } from './$types';
 export const GET: RequestHandler = async ({ url }) => {
     const searchTerm = url.searchParams.get('q');
 
-    const response = await fetch(`https://justcors.com/tl_c014bfb/https://eduboom.ro/ajax/lessons-search?term=${searchTerm}`);
+    const response = await fetch("https://eduboom.ro/ajax/lessons-search?term=doar", {
+        "headers": {
+          "accept": "application/json, text/javascript, */*; q=0.01",
+          "accept-language": "ro,en-GB;q=0.9,en;q=0.8,en-US;q=0.7",
+          "priority": "u=1, i",
+          "sec-ch-ua": "\"Chromium\";v=\"134\", \"Not:A-Brand\";v=\"24\", \"Microsoft Edge\";v=\"134\"",
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": "\"Windows\"",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "x-requested-with": "XMLHttpRequest"
+        },
+        "referrer": "https://eduboom.ro/",
+        "referrerPolicy": "strict-origin-when-cross-origin",
+        "body": null,
+        "method": "GET",
+        "mode": "cors",
+        "credentials": "include"
+      });
+
     const jsonEduboom = await response.json();
 
     let returnData = jsonEduboom.map((r: any) => ({
