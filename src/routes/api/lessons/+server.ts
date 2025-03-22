bac594e050bf7a6232b79014a97beb3de6e56f51
Ovidiu Căpraru <85124797+ovidiuux@users.noreply.github.com>
2025-03-23 00:44:34 +0200

    Update +server.ts

--- src/routes/api/lessons/+server.ts
+++ src/routes/api/lessons/+server.ts
@@ -4,7 +4,7 @@ import type { RequestHandler } from './$types';
 export const GET: RequestHandler = async ({ url }) => {
     const searchTerm = url.searchParams.get('q');
 
-    const response = await fetch("https://eduboom.ro/ajax/lessons-search?term=doar", {
+    const response = await fetch("https://justcors.com/tl_c014bfb/https://eduboom.ro/ajax/lessons-search?term=doar", {
         "headers": {
           "accept": "application/json, text/javascript, */*; q=0.01",
           "accept-language": "ro,en-GB;q=0.9,en;q=0.8,en-US;q=0.7",
@@ -15,7 +15,7 @@ export const GET: RequestHandler = async ({ url }) => {
           "sec-fetch-dest": "empty",
           "sec-fetch-mode": "cors",
           "sec-fetch-site": "same-origin",
-          "x-requested-with": "XMLHttpRequest"
+          "x-requested-with": "XMLHttpRequest",
         },
         "referrer": "https://eduboom.ro/",
         "referrerPolicy": "strict-origin-when-cross-origin",
