80376dde5a68d47ed0354cf1e2794131e5aedfad
Ovidiu Căpraru <85124797+ovidiuux@users.noreply.github.com>
2025-03-23 00:31:32 +0200

    Update +server.ts

--- src/routes/api/lessons/+server.ts
+++ src/routes/api/lessons/+server.ts
@@ -1,31 +1,24 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
-import cloudscraper from 'cloudscraper';
 
 export const GET: RequestHandler = async ({ url }) => {
     const searchTerm = url.searchParams.get('q');
 
-    try {
-        const jsonEduboom = await cloudscraper.get(`https://justcors.com/tl_c014bfb/https://eduboom.ro/ajax/lessons-search?term=${searchTerm}`);
+    const response = await fetch(`https://justcors.com/tl_c014bfb/https://eduboom.ro/ajax/lessons-search?term=${searchTerm}`);
+    const jsonEduboom = await response.json();
 
-        const data = JSON.parse(jsonEduboom);
+    let returnData = jsonEduboom.map((r: any) => ({
+        ...r,
+        url: r.url.replace('https://eduboom.ro', '')
+    }));
 
-        let returnData = data.map((r: any) => ({
-            ...r,
-            url: r.url.replace('https://eduboom.ro', '')
-        }));
-
-        if (data[0]?.url === '') {
-            returnData = [];
-        }
-
-        return json(returnData, {
-            headers: {
-                'Cache-Control': 'public, max-age=691200'
-            }
-        });
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        return json({ error: 'Failed to fetch data' }, { status: 500 });
+    if (jsonEduboom[0]?.url === '') {
+        returnData = [];
     }
+
+    return json(returnData, {
+        headers: {
+            'Cache-Control': 'public, max-age=691200'
+        }
+    });
 };
